be80f3e5386f170866398db67e546db97d939bf8
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-07-01 15:43:10 -0300

    config: return string enum intead of int

--- StorageProject.Api/Program.cs
+++ StorageProject.Api/Program.cs
@@ -4,12 +4,14 @@ using StorageProject.Application.Services;
 using StorageProject.Domain.Contracts;
 using StorageProject.Infrasctructure.Data;
 using StorageProject.Infrastructure.Repositories;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+    options=>options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
